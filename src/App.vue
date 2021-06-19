9ab9baeebd568046ac137a68c404015a74daf876
Bury <perezg.josesal@gmail.com>
2021-06-19 15:35:06 -0500

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <h1>Hola</h1>
+  <button @click="fetchData">Fetch Data</button>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      data : ''
+    }
+  },
+  methods:{
+    async fetchData(){
+      let resp = await this.axios.get('https://rickandmortyapi.com/api/character');
+      this.data = resp.data;
+      console.log(this.data['results']);
+    }
+  }
 }
 </script>
 
