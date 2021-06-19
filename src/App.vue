3ef143de08622df6ff0a606cbe4c4494b1e06ef0
Bury <perezg.josesal@gmail.com>
2021-06-19 17:57:30 -0500

    mat added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Hola</h1>
-  <button @click="fetchData">Fetch Data</button>
+  <div class="container">
+    <h1>Hola</h1>
+    <button @click="fetchData" class="btn">Fetch Data</button>
+    <character-card></character-card>
+    <ul class="pagination">
+    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+    <li class="active"><a href="#!">1</a></li>
+    <li class="waves-effect"><a href="#!">2</a></li>
+    <li class="waves-effect"><a href="#!">3</a></li>
+    <li class="waves-effect"><a href="#!">4</a></li>
+    <li class="waves-effect"><a href="#!">5</a></li>
+    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+  </ul>
+  </div>
+  
 </template>
 
 <script>
+import CharacterCard from './CharacterCard.vue';
 export default {
+  components:{
+    CharacterCard
+  },
   data(){
     return{
       data : ''
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import '../node_modules/materialize-css/dist/css/materialize.css'
 const app = createApp(App);
 app.use(VueAxios, axios);
 app.mount('#app')
