7589042b0357da849b8ae5bd3a6ac4b8e3e3354c
Bury <perezg.josesal@gmail.com>
2021-06-20 12:18:38 -0500

    vuex added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
@@ -7,6 +8,13 @@ import '../node_modules/materialize-css/dist/css/materialize.css'
 import MainPage from './Pages/MainPage.vue';
 import CharacterPage from './Pages/CharacterPage.vue';
 
+const store = createStore({
+    state(){
+        return {
+            characterName : ''
+        }
+    }
+});
 const route = createRouter({
     history : createWebHistory(),
     routes : [
@@ -16,5 +24,6 @@ const route = createRouter({
 });
 const app = createApp(App);
 app.use(route);
+app.use(store);
 app.use(VueAxios, axios);
 app.mount('#app')
